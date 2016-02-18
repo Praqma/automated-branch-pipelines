a8563af7c85d501d2168ab88e4ec5c7df0da3171
Martin Mosegaard Amdisen <mam@praqma.net>
2016-02-18 12:27:11 +0100

    Issue #4: Automated test using Git and Bitbucket
    Added shell that executes a Git workflow.
    It assumes everything is running and configured.
    It does not assert anything after running.

---
+++ src/test/bash/bitbucketTest.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Given:
+# Jenkins, the Java service and Bitbucket are all running.
+# Bitbucket is configured with a project, repository and hook.
+#
+# When:
+# A feature branch is created or deleted in the Git repository.
+#
+# Then:
+# Jenkins is triggered via the service to create or delete a pipeline.
+#
+# Assumes the working dir is the project root.
+# This is the case when this script is executed by Gradle.
+
+# Test dir is only used by this test
+TEST_DIR=acceptTest
+# Modify these settings to match the Bitbucket setup
+# Repository settings matches a project with key "TA" (like "Test Abs")
+PROJECT_KEY=ta
+REPO=test-abs
+BITBUCKET_USER=admin
+BITBUCKET_IP=$(docker-machine ip default)
+BITBUCKET_CLONE_URL=http://$BITBUCKET_USER@$BITBUCKET_IP:7990/scm/$PROJECT_KEY/$REPO.git
+
+echo Creating test directory
+mkdir $TEST_DIR
+cd $TEST_DIR
+
+echo Cloning Git repository from Bitbucket
+git clone $BITBUCKET_CLONE_URL
+cd $REPO
+
+echo Pushing a change on master
+# Should not trigger Jenkins
+echo hi > file1
+git add file1
+git commit -m "add file"
+git push origin master
+
+echo Creating a feature branch and pushing to it
+# Should trigger Jenkins
+git checkout -b feature/1337-coolfeature
+echo hi feature > file1
+git add file1
+git commit -m "update file on feature branch"
+git push -u origin feature/1337-coolfeature
+
+echo Merging feature branch to master
+# Should not trigger Jenkins
+git checkout master
+git pull
+git pull origin feature/1337-coolfeature
+git push origin master
+
+echo Deleting feature branch
+# Should trigger Jenkins
+git push origin :feature/1337-coolfeature
+git branch -d feature/1337-coolfeature
+
+echo Deleting test directory
+cd ../..
+rm -rf $TEST_DIR
+
+echo Done
--- src/test/bash/curlTest.sh
+++ src/test/bash/curlTest.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Given:
+# Jenkins and the Java service are running.
+#
+# When:
+# A POST request is sent to the Java service.
+#
+# Then:
+# A job is triggered on Jenkins.
+#
 # Assumes the working dir is the project root.
 # This is the case when this script is executed by Gradle.
 SERVICE_PORT=$(grep "service.port" src/main/resources/automated-branch-pipelines.properties | cut -d "=" -f 2 | tr -d ' ')
